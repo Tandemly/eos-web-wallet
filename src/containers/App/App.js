d86d40e836c42aa2f5c07b6a1a41b7fd911b11bb
stubelshe <stu.belshe@tandem.ly>
2017-10-09 15:43:17 -0500

    Adding custom styles for EOS

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -23,25 +23,27 @@ const App = () => (
             <div className="user-meta">
               <h4 className="title is-4 is-spaced">Hi, Display Name</h4>
               <p className="subtitle is-6"><a href="">Customize your profile</a> | <a href=""><span className="icon-logout"></span></a></p>
-              <a className="settings" href=""><span className="icon-settings"></span></a>
             </div>
           </div>
+          <a className="settings" href=""><span className="icon-settings"></span></a>
           <figure className="image profile-thumbnail">
             <img src="https://avatarfiles.alphacoders.com/696/69632.jpg" alt="Placeholder image" />
           </figure>
         </div>
       </div>
     </section>
-    <section className="columns is-gapless is-fluid">
+    <section className="columns is-variable is-0">
       <div className="column is-narrow is-hidden-mobile">
         <aside className="menu p-lg">
           <div className="-is-logged-in">
-            <p className="title is-1 balance">1.00m</p>
-            <p className="subtitle is-6 full_balance">1,000,000.0080</p>
-            <span className="tag is-primary is-medium change">+27,600</span>
+            <div className="financials u-p3">
+              <p className="title is-1 balance">2.34<span>B</span></p>
+              <p className="subtitle is-6 full-balance">1,000,000.0080</p>
+              <span className="tag is-primary change"><span className="icon-increase"></span>27,600</span>
+            </div>
             <ul className="menu-list">
               <li>
-                <a href="/"><span className="icon-transfer"></span> Transfer</a>
+                <a href="/" className="is-active"><span className="icon-transfer"></span> Transfer</a>
               </li>
               <li>
                 <a href="/transactions"><span className="icon-history"></span> Transaction History</a>
@@ -69,9 +71,9 @@ const App = () => (
       </div>
       <div className="column">
         <div className="menu-closer" role="button" tabIndex="0" />
-        <div className="columns is-gapless">
-          <div className="column is-7">
-            <section className="section">
+        <div className="columns is-desktop content is-variable is-6">
+          <div className="column is-7-desktop is-12-tablet">
+            <section className="u-mb6">
               <h2 className="title is-2">Transfer</h2>
               <p>Move funds to another EOS account.</p>
               <form>
@@ -82,11 +84,10 @@ const App = () => (
                   </div>
                 </div>
                 <div className="field">
-                  <label className="label" htmlFor="amount">Amount</label>
+                  <label className="label" htmlFor="amount">Amount<p className="help"><a>Use Full Balance</a></p></label>
                   <div className="control">
                     <input type="text" name="amount" className="input" />
                   </div>
-                  <p className="help"><a>Use Full Balance</a></p>
                 </div>
                 <div className="field">
                   <label className="label" htmlFor="memo">Memo</label>
@@ -94,129 +95,146 @@ const App = () => (
                     <input type="text" name="memo" className="input" />
                   </div>
                 </div>
-                <div className="field is-grouped">
+                <div className="field is-grouped u-mt4">
                   <div className="control">
-                    <button className="button is-primary">Submit</button>
+                    <button className="button is-large is-primary">Submit</button>
                   </div>
                   <div className="control">
-                    <button className="button is-secondary">Clear</button>
+                    <button className="button is-large is-secondary">Clear</button>
                   </div>
                 </div>
               </form>
             </section>
-            <section className="section">
-              <h3 className="title is-3">Transaction History</h3>
-              <div className="level box is-mobile">
-                <div className="level-left">
-                  <div className="level-item">
-                    <p className="subtitle is-6">July, 25</p>
+            <section>
+              <h3 className="title is-3">Transaction History<p className="help"><a>See All</a></p></h3>
+              <ul>
+                <li className="level box is-mobile">
+                  <div className="level-left">
+                    <div className="level-item has-text-centered">
+                      <div>
+                        <p className="heading">May</p>
+                        <p className="title">11</p>
+                      </div>
+                    </div>
+                    <div className="level-item">
+                      <img className="image" src="/images/male_2.jpg" />
+                    </div>
+                    <div className="level-item">
+                      <p className="subtitle is-6"><a>Brian Dawson</a></p>
+                    </div>
                   </div>
-                  <div className="level-item">
-                    <img className="image is-32x32" src="/images/male_2.jpg" />
+                  <div className="level-right">
+                    <div className="level-item">
+                      <p className="subtitle is-6">$10.00</p>
+                    </div>
+                    <div className="level-item">
+                      <span className="icon-transfer_to"></span>
+                    </div>
                   </div>
-                  <div className="level-item">
-                    <p className="subtitle is-6"><a>Brian Dawson</a></p>
+                </li>
+                <li className="level box is-mobile">
+                  <div className="level-left">
+                    <div className="level-item has-text-centered">
+                      <div>
+                        <p className="heading">Jul</p>
+                        <p className="title">25</p>
+                      </div>
+                    </div>
+                    <div className="level-item">
+                      <img className="image" src="/images/male_2.jpg" />
+                    </div>
+                    <div className="level-item">
+                      <p className="subtitle is-6"><a>Brian Dawson</a></p>
+                    </div>
                   </div>
-                </div>
-                <div className="level-right">
-                  <div className="level-item">
-                    <p className="subtitle is-6">$10.00</p>
+                  <div className="level-right">
+                    <div className="level-item">
+                      <p className="subtitle is-6">$10.00</p>
+                    </div>
+                    <div className="level-item">
+                      <span className="icon-transfer_from"></span>
+                    </div>
                   </div>
-                  <div className="level-item">
-                    <span className="icon-transfer_to"></span>
+                </li>
+                <li className="level box is-mobile">
+                  <div className="level-left">
+                    <div className="level-item has-text-centered">
+                      <div>
+                        <p className="heading">Sep</p>
+                        <p className="title">6</p>
+                      </div>
+                    </div>
+                    <div className="level-item">
+                      <img className="image" src="/images/male_2.jpg" />
+                    </div>
+                    <div className="level-item">
+                      <p className="subtitle is-6"><a>Brian Dawson</a></p>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="level box is-mobile">
-                <div className="level-left">
-                  <div className="level-item">
-                    <p className="subtitle is-6">July, 25</p>
+                  <div className="level-right">
+                    <div className="level-item">
+                      <p className="subtitle is-6">$10.00</p>
+                    </div>
+                    <div className="level-item">
+                      <span className="icon-transfer_to"></span>
+                    </div>
                   </div>
-                  <div className="level-item">
-                    <img className="image is-32x32" src="/images/male_2.jpg" />
+                </li>
+                <li className="level box is-mobile">
+                  <div className="level-left">
+                    <div className="level-item has-text-centered">
+                      <div>
+                        <p className="heading">Dec</p>
+                        <p className="title">14</p>
+                      </div>
+                    </div>
+                    <div className="level-item">
+                      <img className="image" src="/images/male_2.jpg" />
+                    </div>
+                    <div className="level-item">
+                      <p className="subtitle is-6"><a>Brian Dawson</a></p>
+                    </div>
                   </div>
-                  <div className="level-item">
-                    <p className="subtitle is-6"><a>Brian Dawson</a></p>
+                  <div className="level-right">
+                    <div className="level-item">
+                      <p className="subtitle is-6">$10.00</p>
+                    </div>
+                    <div className="level-item">
+                      <span className="icon-transfer_from"></span>
+                    </div>
                   </div>
-                </div>
-                <div className="level-right">
-                  <div className="level-item">
-                    <p className="subtitle is-6">$10.00</p>
+                </li>
+                <li className="level box is-mobile">
+                  <div className="level-left">
+                    <div className="level-item has-text-centered">
+                      <div>
+                        <p className="heading">May</p>
+                        <p className="title">19</p>
+                      </div>
+                    </div>
+                    <div className="level-item">
+                      <img className="image" src="/images/male_2.jpg" />
+                    </div>
+                    <div className="level-item">
+                      <p className="subtitle is-6"><a>Brian Dawson</a></p>
+                    </div>
                   </div>
-                  <div className="level-item">
-                    <span className="icon-transfer_from"></span>
+                  <div className="level-right">
+                    <div className="level-item">
+                      <p className="subtitle is-6">$10.00</p>
+                    </div>
+                    <div className="level-item">
+                      <span className="icon-transfer_to"></span>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="level box is-mobile">
-                <div className="level-left">
-                  <div className="level-item">
-                    <p className="subtitle is-6">July, 25</p>
-                  </div>
-                  <div className="level-item">
-                    <img className="image is-32x32" src="/images/male_2.jpg" />
-                  </div>
-                  <div className="level-item">
-                    <p className="subtitle is-6"><a>Brian Dawson</a></p>
-                  </div>
-                </div>
-                <div className="level-right">
-                  <div className="level-item">
-                    <p className="subtitle is-6">$10.00</p>
-                  </div>
-                  <div className="level-item">
-                    <span className="icon-transfer_to"></span>
-                  </div>
-                </div>
-              </div>
-              <div className="level box is-mobile">
-                <div className="level-left">
-                  <div className="level-item">
-                    <p className="subtitle is-6">July, 25</p>
-                  </div>
-                  <div className="level-item">
-                    <img className="image is-32x32" src="/images/male_2.jpg" />
-                  </div>
-                  <div className="level-item">
-                    <p className="subtitle is-6"><a>Brian Dawson</a></p>
-                  </div>
-                </div>
-                <div className="level-right">
-                  <div className="level-item">
-                    <p className="subtitle is-6">$10.00</p>
-                  </div>
-                  <div className="level-item">
-                    <span className="icon-transfer_from"></span>
-                  </div>
-                </div>
-              </div>
-              <div className="level box is-mobile">
-                <div className="level-left">
-                  <div className="level-item">
-                    <p className="subtitle is-6">July, 25</p>
-                  </div>
-                  <div className="level-item">
-                    <img className="image is-32x32" src="/images/male_2.jpg" />
-                  </div>
-                  <div className="level-item">
-                    <p className="subtitle is-6"><a>Brian Dawson</a></p>
-                  </div>
-                </div>
-                <div className="level-right">
-                  <div className="level-item">
-                    <p className="subtitle is-6">$10.00</p>
-                  </div>
-                  <div className="level-item">
-                    <span className="icon-transfer_to"></span>
-                  </div>
-                </div>
-              </div>
-              <div className="control">
-                <button className="button is-primary">View All</button>
+                </li>
+              </ul>
+              <div className="control u-mt4">
+                <button className="button is-large is-primary">View All</button>
               </div>
             </section>
           </div>
-          <div className="column is-5">
+          <div className="column is-5-desktop is-12-tablet">
             <section className="section">
               <h5 className="title is-5">What are EOS tokens?</h5>
               <div className="box">
@@ -224,6 +242,8 @@ const App = () => (
                 Ethereum blockchain pursuant to a related ERC-20 smart
                 contract (the “EOS Tokens”).
               </div>
+            </section>
+            <section className="section">
               <h5 className="title is-5">How can I use EOS tokens?</h5>
               <div className="box">
                 block.one is building the EOS.IO Software but it will not
@@ -235,13 +255,13 @@ const App = () => (
                 supplement the EOS.IO Software prior to, during or after
                 launching the EOS Platform.
               </div>
-            </section>
+            </section>  
           </div>
         </div>
-        <footer className="section">
-          <div className="columns">
+        <footer>
+          <div className="columns u-pt2 is-variable is-3">
             <div className="column has-text-left-tablet has-text-centered-mobile">
-              <small>Copyright 2017 | All Rights Reserved</small>
+              <p>Copyright 2017 | All Rights Reserved</p>
             </div>
             <div className="column has-text-right-tablet has-text-centered-mobile">
               <a href="/privacy">Privacy Policy</a>
