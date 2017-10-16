804c6321a136614b54c9f2b877ff2c9b3942d1df
stubelshe <stu.belshe@tandem.ly>
2017-10-16 16:28:39 -0500

    User list styles

--- src/containers/Users/User.js
+++ src/containers/Users/User.js
@@ -2,19 +2,15 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 
 const User = ({ icon, key, name = "", status, url }) => (
-  <li key={key}>
-    <div>
-      <Link to={url} />
-
-      <div className="user-meta d-flex flex-row items-center">
-        {/* <Icon
-          className="transaction-thumbnail"
-          url={icon}
-          /> */}
-
-        <div className="transaction-data ml-3">
-          <div className="transaction-sender mb-1"><a>{name}Demo Name</a></div>
-          <div className="transaction-memo mb-0">{status}</div>
+  <li className="level box user is-mobile" key={key}>
+    <div className="level-left">
+      <div className="level-item">
+        <img className="image" src="/images/male_2.jpg" />
+      </div>
+      <div className="level-item">
+        <div>
+          <p className="username"><a>{name}</a></p>
+          <p className="memo">Memo</p>
         </div>
       </div>
     </div>
