c4ee5a28e498dcde88131f1064b37f722991304c
Orlando La <ceanela@users.noreply.github.com>
2017-10-17 08:57:54 -0500

    Removing unused items

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -25,7 +25,7 @@ import {
   toggleMenu,
   closeMenu,
   routeLoad
-} from "./reducer";
+} from "./actions";
 
 import "./App.scss";
 
--- src/util/configureStore.js
+++ src/util/configureStore.js
@@ -7,21 +7,17 @@ import { persistStore, autoRehydrate } from 'redux-persist';
 import { combineReducers } from 'redux';
 import { reducer as reduxFormReducer } from 'redux-form';
 import { reducer as account } from 'containers/Balance/reducer';
-import { reducer as app } from 'containers/App/reducer';
 import { reducer as login } from 'containers/Login/reducer';
 import { reducer as notification } from 'containers/Notifications/reducer';
 import { reducer as transactions } from 'containers/Transactions/reducer';
-import { reducer as user } from 'containers/User/reducer';
 import { reducer as users } from 'containers/Users/reducer';
 
 const reducers = combineReducers({
-  app,
   account,
   form: reduxFormReducer,
   login,
   notification,
   transactions,
-  user,
   users,
 });
 
