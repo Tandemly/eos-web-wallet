eb4653f1ac1bb53190aa2d185153c24ce1d4789a
Orlando La <ceanela@users.noreply.github.com>
2017-10-17 09:01:27 -0500

    Remove missing reducer and update app

--- src/containers/App/reducer.js
+++ src/containers/App/reducer.js
@@ -2,6 +2,17 @@ const TOGGLE_MENU = 'TOGGLE_MENU';
 const CLOSE_MENU = 'CLOSE_MENU';
 const ROUTE_LOAD = 'ROUTE_LOAD';
 
+const initialState = {
+  isMenuOpen: false,
+};
+
+export function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
 export function toggleMenu() {
   return {
     type: TOGGLE_MENU,
