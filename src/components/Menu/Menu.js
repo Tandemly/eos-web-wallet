4931fb2c00e818fe226e826d6bdb24da8d2d3c98
Orlando La <ceanela@users.noreply.github.com>
2017-10-20 11:24:59 -0500

    Fix recursive list bug

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,14 +3,56 @@ import Balance from "containers/Balance";
 import Login from "containers/Login";
 import Shortcuts from "components/Shortcuts";
 
+
+const unauthLinks = [
+  {
+    to: '/users',
+    text: 'Users',
+  },
+  {
+    to: '/about',
+    text: 'About',
+  },
+  {
+    to: '/faq',
+    text: 'FAQ',
+  },
+];
+const authLinks = [
+  [
+    {
+      to: '/',
+      text: 'Transfer',
+      iconClass: 'icon-transfer u-mr1',
+    },
+    {
+      to: '/transactions',
+      text: 'Transaction History',
+      iconClass: 'icon-history u-mr1',
+    },
+    {
+      to: '/permissions',
+      text: 'Permissions',
+      iconClass: 'icon-permissions u-mr1',
+    },
+    {
+      to: '/logout',
+      text: 'Logout',
+      iconClass: 'icon-logout u-mr1',
+    },
+  ],
+  ...unauthLinks,
+];
+
+
 const Menu = ({ isAuthenticated }) => (
   <div className="menu p-lg">
     {!isAuthenticated && <Login />}
 
     {isAuthenticated && <Balance />}
 
-    <Shortcuts
-      isAuthenticated={isAuthenticated}
+    <Shortcuts 
+      data={isAuthenticated ? authLinks : unauthLinks}
     />
   </div>
 );
--- src/components/Shortcuts/Shortcuts.js
+++ src/components/Shortcuts/Shortcuts.js
@@ -4,60 +4,16 @@ import Shortcut from "./Shortcut";
 import cx from "classnames";
 import css from "./styles.module.scss";
 
-const activeClassName = css.active;
-const links = [
-  [
-    {
-      to: '/',
-      text: 'Transfer',
-      iconClass: 'icon-transfer u-mr1',
-      activeClassName,
-    },
-    {
-      to: '/transactions',
-      text: 'Transaction History',
-      iconClass: 'icon-history u-mr1',
-      activeClassName,
-    },
-    {
-      to: '/permissions',
-      text: 'Permissions',
-      iconClass: 'icon-permissions u-mr1',
-      activeClassName,
-    },
-    {
-      to: '/logout',
-      text: 'Logout',
-      iconClass: 'icon-logout u-mr1',
-      activeClassName,
-    },
-  ],
-  {
-    to: '/users',
-    text: 'Users',
-    activeClassName,
-  },
-  {
-    to: '/about',
-    text: 'About',
-    activeClassName,
-  },
-  {
-    to: '/faq',
-    text: 'FAQ',
-    activeClassName,
-  },
-];
-
 const Shortcuts = ({
-  className,
   data,
 }) => (
-  <List
-    className={cx("menulist", css.menulist)}
-    data={data || links}
-    renderItem={Shortcut}
-  />
+  <div>
+    <List
+      className={cx("menulist", css.menulist)}
+      data={data}
+      renderItem={Shortcut}
+    />
+  </div>
 );
 
 export default Shortcuts;
