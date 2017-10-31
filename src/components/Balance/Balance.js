ccad3441b1c214b6f084866e82a56c85ca514ef5
Orlando La <ceanela@users.noreply.github.com>
2017-10-31 08:35:43 -0700

    Update difference not to show if non-existent

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -8,7 +8,7 @@ const Balance = ({ total, difference, symbol /* , account_name */ }) => (
     <div className={cx("financials u-p3", css.financials)}>
       <p className={cx("title is-1", css.balance)}>{numeral(total).format('0.00a')}<span>B</span></p>
       <p className={cx("full_balance is-6", css.full_balance)}>{numeral(total).format('0,0[.]0000')}</p>
-      {difference && <div className={cx("tag change", css.tag)}>{symbol}{difference}</div>}
+      {!!difference && <div className={cx("tag change", css.tag)}>{symbol}{difference}</div>}
     </div>
   </div>
 );
