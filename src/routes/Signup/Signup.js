6192a3df30bd192710f96479de52c8defcf8ec33
Orlando La <ceanela@users.noreply.github.com>
2017-11-02 08:02:11 -0700

    Fix modal close on signup, rename component

--- src/routes/Signup/Signup.js
+++ src/routes/Signup/Signup.js
@@ -1,44 +1,25 @@
 import * as React from "react";
 import Notifications from "containers/Notifications";
-import Progress from "components/Progress";
 
-const steps = {
-  '/signup': 1,
-  '/signup/email': 2,
-  '/signup/phone': 3,
-  '/signup/complete': 4
-};
+const Signup = ({ children, handleClose }) => (
+  <div className="login">
+    <div className="login-header modal-header">
+      <h3 className="title is-3">Create your Wallet account</h3>
 
-const CreateAccount = ({
-  children,
-  location,
-  handleClose,
-}) => {
-  const step = Object.keys(steps)
-    .reduce((acc, path) => (path === location.pathname ? steps[path] : acc), 1);
-
-  return (
-    <div className="login">
-      <div className="login-header modal-header">
-        <h3 className="title is-3">Create your Wallet account</h3>
-
-        <button
-          className="js-modal-close"
-          onClick={handleClose}
-        >
-          x
-        </button>
-      </div>
-
-      <div className="modal-body">
-        <Notifications />
-
-        <Progress step={step} />
-
-        {children}
-      </div>
+      <button
+        className="js-modal-close"
+        onClick={handleClose}
+      >
+        x
+      </button>
     </div>
-  );
-};
 
-export default CreateAccount;
+    <div className="modal-body">
+      <Notifications />
+
+      {children}
+    </div>
+  </div>
+);
+
+export default Signup;
