d8c5d0121330412ca9be1d32fbeb8d9d531759d3
Michael Kimberlin <mkimberlin@gmail.com>
2017-11-06 14:26:00 -0600

    fix a test and an eslint warning

--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -1,5 +1,4 @@
-import * as React from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import Users from "components/Users";
 
 const Container = connect()(Users);
