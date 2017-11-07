8ebea32dd15b37e6ef8644fa12d3f701a14c5c64
Michael Kimberlin <mkimberlin@gmail.com>
2017-11-07 14:25:54 -0600

    fix snapshot test

--- src/components/Profile/test.js
+++ src/components/Profile/test.js
@@ -1,27 +1,26 @@
 import * as React from "react";
-import renderer from 'react-test-renderer';
-import { StaticRouter } from 'react-router';
-import Profile from './';
+import renderer from "react-test-renderer";
+import { StaticRouter } from "react-router";
+import Profile from "./";
 
-describe('<Profile />', () => {
-  it('renders without crashing', () => {
+describe("<Profile />", () => {
+  it("renders without crashing", () => {
     const staticContext = {};
     const profile = {
-      image: {
-        url: 'src',
-      },
-      currentLocation: 'Friday Harbor, WA',
-      name: 'Rick Sanchez',
-      status: 'active',
+      imageUrl: "src",
+      currentLocation: "Friday Harbor, WA",
+      displayName: "Rick Sanchez",
+      status: "active"
     };
 
-    const tree = renderer.create(
-      <StaticRouter location="/" context={staticContext}> 
-        <Profile {...profile} />
-      </StaticRouter>
-    ).toJSON();
+    const tree = renderer
+      .create(
+        <StaticRouter location="/" context={staticContext}>
+          <Profile {...profile} />
+        </StaticRouter>
+      )
+      .toJSON();
 
     expect(tree).toMatchSnapshot();
   });
-})
-
+});
