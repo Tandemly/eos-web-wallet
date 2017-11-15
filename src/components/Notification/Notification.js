2d6a6f16bd874912a19712102ce70fd8059e0b96
Michael Kimberlin <mkimberlin@gmail.com>
2017-11-15 16:26:49 -0600

    use the appropriate icon for the given notification type

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,11 +1,27 @@
+//@flow
 import * as React from "react";
 import cx from "classnames";
 import css from "./styles.module.scss";
 
-const Notification = ({ text, status, unsetNotification }) =>
+type Status = "success" | "info" | "error" | "warn";
+
+type Props = {
+  text: string,
+  status?: Status,
+  unsetNotification?: () => mixed
+};
+
+const icons = {
+  success: "icon-success",
+  info: "icon-info",
+  error: "icon-fail_warn",
+  warn: "icon-fail_warn"
+};
+
+const Notification = ({ text, status = "info", unsetNotification }: Props) =>
   !status ? null : (
     <div className={cx(css.notification, css[status])}>
-      <div className={cx("icon-success", css.icon)} />
+      <div className={cx(icons[status], css.icon)} />
       {text}
       {unsetNotification && (
         <span className={cx("icon-x", css.close)} onClick={unsetNotification} />
