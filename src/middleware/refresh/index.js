ca9de4596fb9f21523845b2a28c57435eae57cdd
Michael Kimberlin <mkimberlin@gmail.com>
2017-11-27 11:16:09 -0600

    change the refresh logic to avoid delay issue

--- src/middleware/refresh/index.js
+++ src/middleware/refresh/index.js
@@ -3,11 +3,21 @@ import { getTransactions } from "thunks/transactions";
 import { getBalance } from "thunks/balance";
 import { SUCCEED_LOGIN } from "../../redux-modules/user/user-actions";
 import { SUCCESS_POST_TRANSACTION } from "../../redux-modules/transfer/transfer-actions";
-import { selectEOSAccountName } from "../../redux-modules/eos-account/account-selectors";
+import {
+  selectEOSAccountName,
+  selectEOSAccountRehydrated
+} from "../../redux-modules/eos-account/account-selectors";
 import { selectWalletUserAuthenticated } from "../../redux-modules/user/user-selectors";
 import { SET_EOS_ACCOUNT_NAME } from "../../redux-modules/eos-account/account-actions";
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+function refreshAccount(store, eosAccountName) {
+  delay(1000).then(() => {
+    store.dispatch(getBalance(eosAccountName));
+    store.dispatch(getTransactions(eosAccountName));
+  });
+}
 
 // Dispatches action after events
 const refresh = store => next => action => {
@@ -31,13 +41,17 @@ const refresh = store => next => action => {
     isAuthenticated
   ) {
     // NOTE it may take up to 3 seconds for a new transaction to process on the blockchain
-    delay(1000).then(() => {
-      store.dispatch(getBalance(eosAccountName));
-      store.dispatch(getTransactions(eosAccountName));
-    });
+    refreshAccount(store, eosAccountName);
   }
 
   next(action);
+
+  if (
+    action.type === "persist/REHYDRATE" &&
+    selectEOSAccountRehydrated(store.getState())
+  ) {
+    refreshAccount(store, selectEOSAccountName(store.getState()));
+  }
 };
 
 export default refresh;
