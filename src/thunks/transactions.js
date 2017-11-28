f509508a990f3f3175e36f3a3bd0c5167ef2b080
David Atchley <david.m.atchley@gmail.com>
2017-11-28 10:50:14 -0600

    fix: corrected filter/sort on transaction history

--- src/thunks/transactions.js
+++ src/thunks/transactions.js
@@ -3,13 +3,15 @@ import {
   succeedGetTransactions,
   failGetTransactions
 } from "redux-modules/transactions/transactions-actions";
+import { selectEOSAccountName } from "redux-modules/eos-account/account-selectors";
 import { apiRequest } from "../util/fetchUtil";
 import { tryGetTransactions } from "../redux-modules/transactions/transactions-actions";
 
-export const getTransactions = accountName => async dispatch => {
+export const getTransactions = accountName => async (dispatch, getState) => {
+  const account = selectEOSAccountName(getState());
   dispatch(tryGetTransactions(accountName));
   try {
-    const response = await apiRequest("/v1/transactions?sort=-sequence_num&limit=100&filter={\"scope.1\": { \"$exists\": true }}");
+    const response = await apiRequest(`/v1/transactions?sort=-createdAt&limit=50&skip=0&filter={"scope": { "$in": ["${account}"] }}`);
     const transactions = response
       ? response.map(transaction => camelcaseObject(transaction))
       : [];
