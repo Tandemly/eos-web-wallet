e5b5a4480744643ee086007f6d926323c8cf7f4d
Michael Kimberlin <mkimberlin@gmail.com>
2017-12-22 14:21:24 -0600

    close the menu when navigating in mobile

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,39 +1,47 @@
 import * as React from "react";
 import { connect } from "react-redux";
+import { push } from "react-router-redux";
 import { withRouter } from "react-router-dom";
 import { CurrentEOSBalance } from "../Balance";
 import Login from "components/LoginForm/index";
 import Shortcuts from "components/Shortcuts";
 import { doLogout } from "../../thunks/login";
+import { closeMenu } from "../../redux-modules/app/app-actions";
 
-const unauthLinks = [
+const unauthLinks = ({ goTo }) => [
   {
     to: "/users",
+    onClick: goTo,
     text: "Users"
   },
   {
     to: "/about",
+    onClick: goTo,
     text: "About"
   },
   {
     to: "/faq",
+    onClick: goTo,
     text: "FAQ"
   }
 ];
-const authLinks = ({ onLogout }) => [
+const authLinks = ({ onLogout, goTo }) => [
   [
     {
       to: "/transfer",
+      onClick: goTo,
       text: "Transfer",
       iconClass: "icon-transfer u-mr1"
     },
     {
       to: "/transactions",
+      onClick: goTo,
       text: "Transaction History",
       iconClass: "icon-history u-mr1"
     },
     {
       to: "/accounts",
+      onClick: goTo,
       text: "Accounts",
       iconClass: "icon-account u-mr1"
     },
@@ -44,7 +52,7 @@ const authLinks = ({ onLogout }) => [
       iconClass: "icon-logout u-mr1"
     }
   ],
-  ...unauthLinks
+  ...unauthLinks({ goTo })
 ];
 
 const Menu = props => {
@@ -55,13 +63,19 @@ const Menu = props => {
 
       {isAuthenticated && <CurrentEOSBalance />}
 
-      <Shortcuts data={isAuthenticated ? authLinks(props) : unauthLinks} />
+      <Shortcuts
+        data={isAuthenticated ? authLinks(props) : unauthLinks(props)}
+      />
     </div>
   );
 };
 
 const mapDispatchToProps = dispatch => ({
-  onLogout: () => dispatch(doLogout())
+  onLogout: () => dispatch(doLogout()),
+  goTo: e => {
+    dispatch(push(e.target.href));
+    dispatch(closeMenu());
+  }
 });
 
 export default withRouter(connect(undefined, mapDispatchToProps)(Menu));
